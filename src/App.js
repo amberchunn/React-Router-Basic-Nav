6253a18d382e1c400d8348b53b8ae780a827f743
Amber Chunn <a.77@me.com>
2020-03-09 20:03:50 -0700

    Declared Routes for About, Contact, and Home Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
-
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
-	<div>
+	<div className="container">
 		<Navigation />
+		<Route exact path="/">
+			<Home />
+		</Route>
+		<Route path="/About">
+			<About />
+		</Route>
+		<Route path="/Contact">
+			<Contact />
+		</Route>
 	</div>
 );
 
